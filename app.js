e92059090ca722a772abf2c1ba2b4c74aeb14a84
princemaple <chenpaul914@gmail.com>
2013-11-09 18:45:04 +1100

    ok, this works

--- app.js
+++ app.js
@@ -3,36 +3,15 @@
 	return {
 		defaultState: 'debug',
 
-		requests: {
-			fetchWeather: {
-				url: helpers.fmt('http://api.openweathermap.org/data/2.5/forecast/daily?q=%@&mode=json&units=metric&cnt=3', this.setting('city').toLowerCase())
-			}
-		},
+		requests: {},
 
 		events: {
-			'app.activated': 'debug',
-
-			'requestWeather.done': function(data){
-				this.renderWeather(data || {});
-			},
-
-			'requestWeather.fail': function(data){
-				this.switchTo('fetch_fail');
-			}
+			'app.activated': 'debug'
 		},
 
 		debug: function(){
 			alert("started");
 			console.log("launched");
-		},
-
-		requestWeather: function(){
-			this.ajax('fetchWeather');
-		},
-
-		renderWeather: function(data){
-			this.weatherData = data;
-			console.log(data);
 		}
 	};
 
